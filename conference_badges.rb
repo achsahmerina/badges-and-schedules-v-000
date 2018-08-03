163387741ff2a49751633765c3c10375f8b4a383
Achsah Abraham <achsahmp@gmail.com>
2018-08-03 20:19:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  name = "Arel"
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    new_array = []
+      array.each do |name|
+        new_array.push("Hello, my name is #{name}.")
+      end
+      return new_array
+end
+
+def assing_rooms(name, number)
+  new_array = []
+  counter = 1
+    array.each do |name|
+      new_array.push("Hello, #{name}! You'll be assignmed to room #{counter}!")
+      counter += 1
+    end
+    return new_array
+  end
